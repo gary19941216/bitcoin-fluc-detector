2682c939cb3eaed2ef2397ac7832450f5baf40be
Ubuntu <ubuntu@ip-10-0-0-11.us-west-2.compute.internal>
2020-06-02 22:44:50 +0000

    add loadAvro function

--- batch_processing/src/main/scala/dataloader.scala
+++ batch_processing/src/main/scala/dataloader.scala
@@ -1,15 +1,22 @@
 package dataload
 
 import org.apache.spark.sql._
+import org.apache.spark.rdd.RDD
 
 class DataLoader(val spark: SparkSession)
 {
     private var data : Dataset[Row] = _  
 
-    //load data from file
-    def load(path: String) : Unit =
+    //load json data from file
+    def loadJson(path: String) : Unit =
     {    
         data = spark.read.json(path)
+    }
+
+    //load avro data from file
+    def loadAvro(path: String) : Unit = 
+    {
+        data = spark.read.format("avro").load(path)
     }
 
     //retrieve data
@@ -24,16 +31,17 @@ class DataLoader(val spark: SparkSession)
         data.write.mode("append").parquet(path)
     }
 
+    //print DataFrame Schema
     def printSchema() : Unit =
     {
         data.printSchema()
     }
 
+    //show content in DataFrame
     def showContent() : Unit = 
     {
         data.show()
     }
-
+    
 }
-
 
--- batch_processing/src/main/scala/main.scala
+++ batch_processing/src/main/scala/main.scala
@@ -1,5 +1,8 @@
 package bitfluc
 
+import sys.process._
+import java.net.URL
+import java.io.File
 import org.apache.spark.sql.SparkSession
 import dataload.DataLoader
 
@@ -10,32 +13,24 @@ object BitFluc
         val spark = getSparkSession()
         val dataLoader = new DataLoader(spark)
 
-        val inputPath = "s3a://gary-reddit-json/comments/comments_2011_05.json"
+        val inputPath = "https://storage.googleapis.com/gary_bitcoin_test/bitcoin_data-000000000000.avro"
+        //val inputPath = "s3a://gary-reddit-json/comments/comments_2011_05.json"
         val outputPath = "s3a://gary-reddit-parquet/comments"
 
-        dataLoader.load(inputPath)
+        //dataLoader.loadURL(url)
+        
+        //dataLoader.showContent()
+        //dataLoader.loadJson(inputPath) 
+        dataLoader.loadAvro(inputPath)
         dataLoader.printSchema()
-        dataLoader.showContent()
-        dataLoader.writeParquet(outputPath)                                
+        /*dataLoader.showContent()
+        dataLoader.writeParquet(outputPath)*/                                
     }
 
     def getSparkSession(): SparkSession =  
     {
 	val spark = SparkSession.builder
           .appName("Bit Fluc")
-          .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-	  .config("com.amazonaws.services.s3a.enableV4","true")
-	  .config("spark.driver.extraJavaOptions", "-Dcom.amazonaws.services.s3.enableV4=true")
-          .config("spark.executor.extraJavaOptions", "-Dcom.amazonaws.servises.s3.enableV4=true")
-	  .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-          .config("spark.hadoop.fs.s3a.access.key",sys.env.getOrElse("AWS_ACCESS_KEY_ID", ""))
-          .config("spark.hadoop.fs.s3a.secret.key",sys.env.getOrElse("AWS_SECRET_ACCESS_KEY", ""))
-          .config("spark.hadoop.fs.s3a.endpoint","s3.us-west-2.amazonaws.com")
-	  .config("spark.hadoop.fs.s3a.multiobjectdelete.enable","false")
-          .config("spark.hadoop.fs.s3a.fast.upload","true")
-          .config("spark.sql.parquet.filterPushdown", "true")
-          .config("spark.sql.parquet.mergeSchema", "false")
-          .config("spark.hadoop.mapreduce.fileoutputcommitter.algorithm.version", "2")
           .getOrCreate()
 	 
 	return spark 
