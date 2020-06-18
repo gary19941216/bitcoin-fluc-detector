b4a10aea71861551f68b78127471246a3e34ffcd
Ubuntu <ubuntu@ip-10-0-0-6.us-west-2.compute.internal>
2020-06-18 00:44:04 +0000

    dash intermediate version

--- dashboard/app.py
+++ dashboard/app.py
@@ -16,6 +16,9 @@ from cassandra.query import tuple_factory
 from cassandra import ConsistencyLevel
 import cassandra.util
 
+from pyorbital.orbital import Orbital
+satellite = Orbital('TERRA')
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 server = flask.Flask(__name__)
@@ -27,14 +30,8 @@ def pandas_factory(colnames, rows):
 def to_str(date):
     return str(date)
 
-def to_str(date, hour):
+def to_str_hour(date, hour):
     return str(date) + " {hour:02d}".format(hour=hour)
-
-def to_str(date, hour, minute):
-    return str(date) + " {hour:02d}".format(hour=hour) + ":{minute:02d}".format(minute=minute)
-
-def to_str(date, hour, minute, second):
-    return str(date) + " {hour:02d}".format(hour=hour) + ":{minute:02d}".format(minute=minute) + ":{second:02d}".format(second=second)
 
 def normalize_hundred(score):
     if score*100 > 10000: return score
@@ -44,31 +41,41 @@ def normalize_ten(score):
     if score*10 > 10000: return score
     else: return score*10
 
-def get_df(time, subreddit, sentiment):
+def current_get_df():
+
+    bitcoin_query = "SELECT date, hour, price FROM bitcoin_streaming_test_new"
+    reddit_query = "SELECT date, hour, score FROM reddit_streaming_test_new"
+
+    bitcoin_df = session.execute(bitcoin_query)._current_rows
+    reddit_df = session.execute(reddit_query)._current_rows
+
+    bitcoin_df['date'] = bitcoin_df.apply(lambda x: to_str_hour(x.date, x.hour), axis = 1)
+    reddit_df['date'] = reddit_df.apply(lambda x: to_str_hour(x.date, x.hour), axis = 1)
+
+    bitcoin_reddit_df = pd.merge(left=bitcoin_df, right=reddit_df, left_on='date', right_on='date')
+
+    print(bitcoin_reddit_df)
+
+    bitcoin_reddit_df = bitcoin_reddit_df.sort_values('date')
+
+    return bitcoin_reddit_df
+
+def past_get_df(time, subreddit, sentiment):
    
-    '''if time == "five_day" or time == "current":
-        query = "SELECT date, hour, minute, price, score FROM reddit_bitcoin_{time}_{subreddit}_{sentiment}".format(time=time, subreddit=subreddit, sentiment=sentiment)
-    elif time == "six_month" or time == "three_month" or time == "one_month":
-        query = "SELECT date, hour, price, score FROM reddit_bitcoin_{time}_{subreddit}_{sentiment}".format(time=time, subreddit=subreddit, sentiment=sentiment)
+    if time == "one_month" or time == "five_day":
+        query = "SELECT date, hour, price, score FROM {time}_{subreddit}_{sentiment}".format(time=time, subreddit=subreddit, sentiment=sentiment)
     else:
-        query = "SELECT date, price, score FROM reddit_bitcoin_{time}_{subreddit}_{sentiment}".format(time=time, subreddit=subreddit, sentiment=sentiment)'''
+        query = "SELECT date, price, score FROM {time}_{subreddit}_{sentiment}".format(time=time, subreddit=subreddit, sentiment=sentiment)
 
-    query = "SELECT date, hour, minute, second, score FROM reddit_streaming_test3"
-    time = "current"
-
+    print(query)
     bitcoin_reddit_rows = session.execute(query)
     bitcoin_reddit_df = bitcoin_reddit_rows._current_rows
 
-    if time == "current":
-        bitcoin_reddit_df['date'] = bitcoin_reddit_df.apply(lambda x: to_str(x.date, x.hour, x.minute, x.second), axis = 1)
-    elif time == "five_day":
-        bitcoin_reddit_df['date'] = bitcoin_reddit_df.apply(lambda x: to_str(x.date, x.hour, x.minute), axis = 1)
-        #bitcoin_reddit_df['score'] = bitcoin_reddit_df['score'].apply(normalize_hundred)
-    elif time == "six_month" or time == "three_month" or time == "one_month":
-        bitcoin_reddit_df['date'] = bitcoin_reddit_df.apply(lambda x: to_str(x.date, x.hour), axis = 1)
-        #bitcoin_reddit_df['score'] = bitcoin_reddit_df['score'].apply(normalize_hundred)
+    if time == "one_month" or time == "five_day":
+        bitcoin_reddit_df['date'] = bitcoin_reddit_df.apply(lambda x: to_str_hour(x.date, x.hour), axis = 1)
     else:
         bitcoin_reddit_df['date'] = bitcoin_reddit_df.apply(lambda x: to_str(x.date), axis = 1)
+    
     bitcoin_reddit_df = bitcoin_reddit_df.sort_values('date')
 
     return bitcoin_reddit_df
@@ -103,20 +110,104 @@ def show_dashboard(session):
                 {'label': 'Bitcoin', 'value': 'bitcoin'},
                 {'label': 'CryptoCurrency', 'value': 'cryptocurrency'},
                 {'label': 'Ethereum', 'value': 'ethereum'},
-                {'label': 'Tether', 'value': 'tether'}
+                {'label': 'Ripple', 'value': 'ripple'}
             ],
             value='all'
         ), 
 	dcc.Dropdown(
             id='sentiment-dropdown',                                                                                                                                     
             options=[
-                {'label': 'No', 'value': 'no_sentiment'},
-                {'label': 'Yes', 'value': 'with_sentiment'},
+                {'label': 'No', 'value': 'no_nlp'},
+                {'label': 'Yes', 'value': 'with_nlp'},
             ],
             value='no_sentiment'
         ),
-        dcc.Graph(id='bitcoin-reddit-graph')
+        dcc.Graph(id='bitcoin-reddit-graph'),
+	dcc.Graph(id='bitcoin-reddit-bar_plot'),
+	dcc.Graph(id='bitcoin-reddit-stream-graph'),
+        dcc.Interval(
+            id='interval-component',
+            interval=1*1000, # in milliseconds
+            n_intervals=0
+        )
     ])    
+
+@app.callback(Output('bitcoin-reddit-stream-graph', 'figure'),
+              [Input('interval-component', 'n_intervals')])
+def update_graph_live(n):
+
+    bitcoin_reddit_df = current_get_df()
+
+    bitcoin = go.Scatter(
+        x=bitcoin_reddit_df['date'],
+        y=bitcoin_reddit_df['price'],
+        name = "Bitcoin",
+        line = dict(color = '#17BECF'),
+        opacity = 0.8)
+
+    reddit = go.Scatter(
+        x=bitcoin_reddit_df['date'],
+        y=bitcoin_reddit_df['score'],
+        name = "Reddit",
+        line = dict(color = '#7F7F7F'),
+        opacity = 0.8)
+
+    data = [bitcoin,reddit]
+
+    layout = dict(
+        title='Bitcoin_Reddit_Real_Time',
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+		    dict(step='all')
+		])
+            ),
+            rangeslider=dict(),
+            type='date'
+        )
+    )
+
+    return {
+        'data' : data,
+        'layout' : layout
+    }
+
+@app.callback(
+    Output('bitcoin-reddit-bar_plot', 'figure'),
+    [Input('time-dropdown', 'value'),
+     Input('subreddit-dropdown', 'value'),                                                                                                                                                        Input('sentiment-dropdown', 'value')]) 
+def update_figure(time, subreddit, sentiment):
+
+    query = "SELECT bitcoin_spike, bitcoin_reddit_spike FROM spike WHERE interval = {time} AND subreddit = {subreddit} AND nlp = {sentiment}"\
+             .format(time=time, subreddit=subreddit, sentiment=sentiment)
+
+    spike_rows = session.execute(query)
+    
+    #print(
+
+    reddit_relevant_count = bitcoin_reddit_spike
+    reddit_unrelevant_count = bitcoin_spike - bitcoin_reddit_spike
+
+    reddit_relevant = go.Bar(
+        x=['spike_count'],
+        y=[reddit_relevant_count],
+        name='reddit_relevant'
+    )
+    
+    reddit_unrelevant = go.Bar(
+        x=['spike_count'],
+        y=[reddit_unrelevant_count],
+        name='other'
+    )
+
+    data = [reddit_relevant, reddit_unrelevant]
+
+    layout=go.Layout(barmode='stack')
+
+    return {
+        'data' : data,
+        'layout' : layout
+    }
 
 @app.callback(
     Output('bitcoin-reddit-graph', 'figure'),
@@ -125,14 +216,14 @@ def show_dashboard(session):
      Input('sentiment-dropdown', 'value')])
 def update_figure(time, subreddit, sentiment):
 
-    bitcoin_reddit_df = get_df(time, subreddit, sentiment)
+    bitcoin_reddit_df = past_get_df(time, subreddit, sentiment)
 
-    '''bitcoin = go.Scatter(
+    bitcoin = go.Scatter(
         x=bitcoin_reddit_df['date'],
         y=bitcoin_reddit_df['price'],
         name = "Bitcoin",
         line = dict(color = '#17BECF'),
-        opacity = 0.8)'''
+        opacity = 0.8)
 
     reddit = go.Scatter(
         x=bitcoin_reddit_df['date'],
@@ -141,33 +232,16 @@ def update_figure(time, subreddit, sentiment):
         line = dict(color = '#7F7F7F'),
         opacity = 0.8)    
 
-    data = [reddit]
+    data = [bitcoin,reddit]
         
     layout = dict(
         title='Bitcoin_Reddit_Relation',
         xaxis=dict(
             rangeselector=dict(
                 buttons=list([
-
-                    dict(count=30,
-                         label='1m',
-                         step='hour',
-                         stepmode='backward'),
                     dict(count=180,
                          label='6m',
                          step='day',
-                         stepmode='backward'),
-                    dict(count=365,
-                         label='1y',
-                         step='day',
-                         stepmode='backward'),
-                    dict(count=1095,
-                         label='3y',
-                         step='week',
-                         stepmode='backward'),
-                    dict(count=1825,
-                         label='5y',
-                         step='week',
                          stepmode='backward'),
                     dict(step='all')
                 ])
@@ -176,10 +250,6 @@ def update_figure(time, subreddit, sentiment):
             type='date'
         )
     )
-
-    print(data)
-
-    print(layout)
 
     return {
         'data' : data,
