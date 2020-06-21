8f8ea42058230358f80f4b34da73f728f6f088eb
Ubuntu <ubuntu@ip-10-0-0-11.us-west-2.compute.internal>
2020-06-21 07:42:16 +0000

    add Airflow

---
+++ Airflow/sparkDAG.py
@@ -0,0 +1,33 @@
+
+# airflowRedditPysparkDag.py
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+import os
+
+scalaClass = ' --class unify.Unify'
+packages = ' --packages com.datastax.spark:spark-cassandra-connector_2.11:2.5.0,com.johnsnowlabs.nlp:spark-nlp_2.11:2.4.2,org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.3'
+path = ' /usr/local/Insight_Project/bitcoin-fluc-detector/Spark/target/scala-2.11/bit_fluc_2.11-1.0.jar'
+sparkSubmit = '/usr/local/spark/bin/spark-submit'
+
+## Define the DAG object
+default_args = {
+    'owner': 'insight-dan',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 6, 21),
+    'retries': 5,
+    'retry_delay': timedelta(minutes=1),
+}
+dag = DAG('reddit_bitcoin_DAG', default_args=default_args, schedule_interval=timedelta(10))
+
+command = command=sparkSubmit + scalaClass + packages + path
+print(command)
+
+#task to compute number of unique authors
+unify = BashOperator(
+    task_id='unify-data',
+    bash_command=command,
+    dag=dag)
+#Specify that this task depends on the downloadData task
+#unify.set_upstream(downloadData)
+
